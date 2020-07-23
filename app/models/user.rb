b7bfc8aa4039deae73109d9a1233385b20ac7a85
omatsu71 <jampamam1223@gmail.com>
2020-07-23 16:28:01 +0900

    up

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  validates :email, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :reviews
+
+  has_one_attached :avatar
+
+end
